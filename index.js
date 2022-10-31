d9a0b37ca2a7b91bae27b07667cecbf532590fe6
Neil Gall <neil.gall@contxt.id>
2022-10-31 15:34:52 +0000

    Integrate darkspark plug

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const { darkspark } = require('darkspark-expressjs-plug');
 const app = express()
 const port = 3000
+
+const API_KEY = process.env.DARKSPARK_API_KEY;
+if (!API_KEY) {
+    console.error("Please set DARKSPARK_API_KEY environment variable");
+    process.exit(1);
+}
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 });
+
+darkspark(app, API_KEY);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
